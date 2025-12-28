1a9d9b2102eb1ee61ae88ee17714ebd96902c675
TW0hank0 <248451438+TW0hank0@users.noreply.github.com>
2025-12-28 14:08:19 +0800

    修復：github CI

---
+++ ci/find_exe.py
@@ -0,0 +1,9 @@
+import os
+
+
+def main():
+    dirs = os.listdir(os.path.join(os.path.dirname(__file__), "..", "dist"))
+    for i in dirs:
+        if i.startswith("positive_password_book"):
+            print(i)
+            break
