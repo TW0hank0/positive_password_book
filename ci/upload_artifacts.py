d546ff10a95045d4831d992b5eb9a6c5cc25e02b
TW0hank0 <248451438+TW0hank0@users.noreply.github.com>
2026-01-28 15:00:15 +0800

    ci9

---
+++ ci/upload_artifacts.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import sys
+
+dirs = os.listdir(os.path.join(os.path.dirname(__file__), "arttifacts"))
+for dir in dirs:
+    print(f"dir now: {dir}")
+    if os.path.isfile(dir) is True:
+        print(f"uploading {dir}")
+        full_path = os.path.join(
+            os.path.join(os.path.dirname(__file__), "arttifacts"), dir
+        )
+        subprocess.run(
+            ["gh", "release", "upload", sys.argv[1], dir, f"--repo={sys.argv[2]}"]
+        )
